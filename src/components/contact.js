77a26eff638912175325deb68911f36d9f175459
akbarrofiqh01 <akbarrofiqhidayat@gmail.com>
2026-03-17 21:40:25 +0700

    awdwad

--- src/components/contact.js
+++ src/components/contact.js
@@ -36,6 +36,7 @@ function AppContact() {
       </Container>
       <div className='google-map'>
         <iframe
+        title='maps'
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63298.61637188583!2d112.66562138826781!3d-7.447153587015802!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7e1343346658b%3A0x96cbcb91139dab36!2sSidoarjo%2C%20Kec.%20Sidoarjo%2C%20Kabupaten%20Sidoarjo%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1773580344098!5m2!1sid!2sid"
           width="100%"
           height="450"
